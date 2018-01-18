9da87efa8ab2c203a1414766341d3ba7ca7f384a
kai <kai@kai-ubuntu.hsd1.ca.comcast.net>
2018-01-17 16:24:31 -0800

    added locate connector C function

--- train_circuit.bat
+++ train_circuit.bat
@@ -1,1 +1,1 @@
-python scripts\train_circuit.py csv C:\Users\Kai\study\robot_prototype\data\MLRotation\annotation6.txt C:\Users\Kai\study\robot_prototype\data\MLRotation\classname6.txt
+python scripts/train_circuit.py csv /media/kai/Data1/robotPrototype/data/MLRotation/annotation6.txt /media/kai/Data1/robotPrototype/data/MLRotation/classname6.txt
