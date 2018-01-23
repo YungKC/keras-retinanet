5910ca0470f5f24779776433e619d7b6775807c3
kai <kai@kai-ubuntu.hsd1.ca.comcast.net>
2018-01-22 17:10:44 -0800

    updated with latest 2d calibration

--- train_circuit.bat
+++ train_circuit.bat
@@ -1,1 +1,1 @@
-python scripts/train_circuit.py csv /media/kai/Data1/robotPrototype/data/MLRotation/annotation6.txt /media/kai/Data1/robotPrototype/data/MLRotation/classname6.txt
+python scripts/train_circuit.py csv /media/kai/Data1/robotPrototype/data/MLRotation/annotation7.txt /media/kai/Data1/robotPrototype/data/MLRotation/classname7.txt
