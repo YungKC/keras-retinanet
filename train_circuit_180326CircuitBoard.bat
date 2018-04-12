f22c8e790ab9744310ac90c617c90014720d8066
kai <kai@kai-ubuntu.hsd1.ca.comcast.net>
2018-04-12 11:52:06 -0700

    update with latest

---
+++ train_circuit_180326CircuitBoard.bat
@@ -0,0 +1,1 @@
+python scripts/train_rfBoard2.py csv /media/kai/Data1/robotPrototype/data/MLRotation/annotation180326CircuitBoard.txt /media/kai/Data1/robotPrototype/data/MLRotation/classname180326CircuitBoard.txt
