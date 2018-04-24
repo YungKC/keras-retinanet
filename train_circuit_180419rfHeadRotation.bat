35f6a0e5f67e6ff8a1d7f1859b08703152cd8431
kai <kai@kai-ubuntu.hsd1.ca.comcast.net>
2018-04-24 11:06:13 -0700

    added rf head rotation training and inference

---
+++ train_circuit_180419rfHeadRotation.bat
@@ -0,0 +1,1 @@
+python scripts/train_rf_rotation.py csv /media/kai/Data1/robotPrototype/data/180419rfHeadRotation/annotationRFHead1.txt /media/kai/Data1/robotPrototype/data/180419rfHeadRotation/classnameRFHead.txt
